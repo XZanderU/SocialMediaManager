e1938160e6647637c6fa1cb3d2de3e21098e031b
Alexander Urrea <ag8388714@gmail.com>
2026-09-19 11:51:49 -0500

    feat: Remocion de <element> en App.jsx ruta payment / y remoción total de index.jsx / (acusaban pantalla en blanco en produccion)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
@@ -11,39 +11,48 @@ import Payment from './pages/Payment';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
-// Llave pública de Stripe (reemplaza con tu propia clave)
+// Llave pública de Stripe
 const stripePromise = loadStripe('tu_llave_publica_de_stripe');
 
 function App() {
   return (
     <AuthProvider>
       <Elements stripe={stripePromise}>
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login />}/>        
-        <Route path='/payment' element={<Elements stripe={stripePromise}><Payment /></Elements>} />
-        <Route path='/dashboard' 
-                element={
+        <Router>
+          <Routes>
+            {/* Redirección automática desde la raíz a Home */}
+            <Route path="/" element={<Navigate to="/Home" replace />} />
+            
+            <Route path="/login" element={<Login />} />        
+            <Route path="/payment" element={<Payment />} />
+            <Route 
+              path="/dashboard" 
+              element={
                 <PrivateRoute>
                   <Dashboard />
-                  </PrivateRoute>
-                }/>
-           <Route path='/profile' 
-               element={
+                </PrivateRoute>
+              }
+            />
+            <Route 
+              path="/profile" 
+              element={
                 <PrivateRoute>
                   <Profile />
                 </PrivateRoute>
-               } />
-           <Route path='/settings' 
-               element={
+              }
+            />
+            <Route 
+              path="/settings" 
+              element={
                 <PrivateRoute>
                   <Settings />
-                  </PrivateRoute>
-               } />
-            <Route path='/Home' element={<Home />} />
-        </Routes>
-    </Router>
-    </Elements>
+                </PrivateRoute>
+              }
+            />
+            <Route path="/Home" element={<Home />} />
+          </Routes>
+        </Router>
+      </Elements>
     </AuthProvider>
   );
 }
